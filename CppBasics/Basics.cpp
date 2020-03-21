f1f98693ed3b8767e1e1c1d378351f119b3ede03
jesussilvag56 <jesussilvag56@gmail.com>
2020-03-20 23:54:46 -0600

    Included output examples

--- CppBasics/Basics.cpp
+++ CppBasics/Basics.cpp
@@ -9,15 +9,18 @@ int main ()
     int c = 0;      //Variable c is direct initialized with 0
     int d;          //Variable d is not Initialized just Defined
 
-    d = 0;          //We assign a value to d
+    d = 0;          //We assigned a value to d
 
     //Variables a, b, and c are initialized (Created with an initial value)
     //Variable d has no inital value but is later Assigned with one. 
 
-    std::cout << "Variable a = " << a << std::endl;
-    std::cout << "Variable b = " << b << std::endl;
-    std::cout << "Variable c = " << c << std::endl;
-    std::cout << "Variable d = " << d << std::endl;
+    std::cout << "Variable a = " << a << "\n";  
+    std::cout << "Variable b = " << b << "\n";
+    std::cout << "Variable c = " << c << "\n";
+    std::cout << "Variable d = " << d << "\n";
+
+    std::cout << "It's always prefered to end a cout with \\n \n";
+    std::cout << "Than just using <<endl after the text" << std::endl;
 
     return 0; 
 }
